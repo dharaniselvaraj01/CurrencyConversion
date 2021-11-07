e131a4ea9f5921791e784c937575007797aa6c70
dharaniselvaraj01 <73701464+dharaniselvaraj01@users.noreply.github.com>
2021-11-07 17:16:56 +0530

    Add files via upload

---
+++ userinp.py
@@ -0,0 +1,15 @@
+from currencybt import currency_list
+
+def get_input():
+    print("From:",end=" ")
+    inp = input()
+    from_curr = inp.split(" ")[0].upper()
+    amt = float(inp.split(" ")[1])
+    print("To:", end=" ")
+    to_curr = input().upper()
+    return from_curr,to_curr,amt
+
+
+
+def check_currency(curr1,curr2):
+    return (curr1 in currency_list) and (curr2 in currency_list)
